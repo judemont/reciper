6a6a728bd0e04c27c3d3d0d1036537be15b43046
JdM <julien@rmbi.ch>
2024-08-28 15:02:15 +0200

    chore: update color scheme in TagsSelector widget

--- lib/widgets/tags_selector.dart
+++ lib/widgets/tags_selector.dart
@@ -41,7 +41,7 @@ class _TagsSelectorState extends State<TagsSelector> {
         children: <Widget>[
           DrawerHeader(
             decoration: BoxDecoration(
-              color: Theme.of(context).colorScheme.secondary,
+              color: Theme.of(context).colorScheme.primary,
             ),
             child: const Text(
               'Tags',
