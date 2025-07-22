bf4f9e52c096ed12886cd338934418fb30b11eab
JdM <julien@rmbi.ch>
2025-07-22 14:45:46 +0200

    removed flex_color_scheme blsht

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:reciper/screens/pages_layout.dart';
 import 'screens/home.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
-import 'package:flex_color_scheme/flex_color_scheme.dart';
 
 Future<void> main() async {
   sqfliteFfiInit();
@@ -19,40 +18,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Reciper',
-        theme: FlexThemeData.light(
-          scheme: FlexScheme.materialBaseline,
-          surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
-          blendLevel: 7,
-          subThemesData: const FlexSubThemesData(
-            blendOnLevel: 10,
-            blendOnColors: false,
-            useTextTheme: true,
-            useM2StyleDividerInM3: true,
-            alignedDropdown: true,
-            useInputDecoratorThemeInDialogs: true,
-          ),
-          visualDensity: FlexColorScheme.comfortablePlatformDensity,
-          useMaterial3: true,
-          swapLegacyOnMaterial3: true,
-          // To use the Playground font, add GoogleFonts package and uncomment
-          // fontFamily: GoogleFonts.notoSans().fontFamily,
+        theme: ThemeData(
+          brightness: Brightness.light,
+          /* light theme settings */
         ),
-        darkTheme: FlexThemeData.dark(
-          scheme: FlexScheme.materialBaseline,
-          surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
-          blendLevel: 13,
-          subThemesData: const FlexSubThemesData(
-            blendOnLevel: 20,
-            useTextTheme: true,
-            useM2StyleDividerInM3: true,
-            alignedDropdown: true,
-            useInputDecoratorThemeInDialogs: true,
-          ),
-          visualDensity: FlexColorScheme.comfortablePlatformDensity,
-          useMaterial3: true,
-          swapLegacyOnMaterial3: true,
-          // To use the Playground font, add GoogleFonts package and uncomment
-          // fontFamily: GoogleFonts.notoSans().fontFamily,
+        darkTheme: ThemeData(
+          brightness: Brightness.dark,
+          /* dark theme settings */
         ),
         home: const PagesLayout(child: HomePage()));
   }
